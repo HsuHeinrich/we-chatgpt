f47cbdb1ba9778135fac88db850624a5ea340754
xuhanyu <xuhanyu@yuanfudao.com>
2023-03-02 15:12:38 +0800

    add

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ def index():
     return 'hello world！'
 
 
-@app.route('/message/post', methods=['GET', 'POST'])
-def handle_wechat_message():
-    signature = request.args.get('signature')
-    timestamp = request.args.get('timestamp')
-    nonce = request.args.get('nonce')
-    echostr = request.args.get('echostr')
+# @app.route('/message/post', methods=['GET', 'POST'])
+# def handle_wechat_message():
+#     signature = request.args.get('signature')
+#     timestamp = request.args.get('timestamp')
+#     nonce = request.args.get('nonce')
+#     echostr = request.args.get('echostr')
     
-    if request.method == 'GET':
-        # 验证服务器
-        return echostr
+#     if request.method == 'GET':
+#         # 验证服务器
+#         return echostr
     
-    if request.method == 'POST':
-        # 处理公众号消息
-        xml = request.stream.read().decode('utf-8')
-        msg = parse_message(xml)
+#     if request.method == 'POST':
+#         # 处理公众号消息
+#         xml = request.stream.read().decode('utf-8')
+#         msg = parse_message(xml)
         
-        if msg.type == 'text':
-            reply = TextReply(content=msg.content, message=msg)
-            return reply.render()
+#         if msg.type == 'text':
+#             reply = TextReply(content=msg.content, message=msg)
+#             return reply.render()
 
 
 if __name__ == "__main__":
